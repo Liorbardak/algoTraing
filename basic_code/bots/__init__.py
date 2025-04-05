0b22b45b9cf4d2614b925af9c1b5cf4ab2de2fcf
LB <dadabardakcomeback@gmail.com>
2025-04-05 15:13:23 +0300

    charny bot

---
+++ basic_code/bots/__init__.py
@@ -0,0 +1,5 @@
+
+__all__ = ['tradeOrder', 'BaseBot' ,'DefaultBot', 'SimpleBot','CharnyBot']
+from .basebot import BaseBot , tradeOrder
+from .simplelstbot import DefaultBot, SimpleBot
+from .charnybot import CharnyBot
--- basic_code/bots/basebot.py
+++ basic_code/bots/basebot.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import numpy as np
+from typing import Dict
+class tradeOrder:
+    def __init__(self, order_type : str , amount : float = np.inf  ):
+        self.order_type = order_type
+        self.amount = amount
+
+
 class BaseBot(object):
-    def __init__(self, name: str = 'base'):
+    def __init__(self, name: str = 'base' , params : Dict = None):
         self._name = name
+        self._params = params
 
     def strategy(self, data: pd.DataFrame):
         '''
@@ -10,6 +18,9 @@ class BaseBot(object):
         :param data:
         :return:
         '''
-        return np.full(len(data), 'sell')
+        return np.full(len(data), tradeOrder('sell'))
+    def display(self, stock_name : str, stock_df: pd.DataFrame ,
+                trade_signal : np.array, reference_index : pd.DataFrame = None)->"fig":
+        return None
 
 
