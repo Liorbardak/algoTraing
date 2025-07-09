31f8d7f90690639f94205abd77ddc93831028872
dadab <dadabardakcomeback@gmail.com>
2025-07-09 08:44:56 +0300

    reorder

--- basic_code/bots/__init__.py
+++
@@ -1,5 +0,0 @@
-
-__all__ = ['tradeOrder', 'BaseBot' ,'DefaultBot', 'SimpleBot','CharnyBot']
-from .basebot import BaseBot , tradeOrder
-from .simplelstbot import DefaultBot, SimpleBot
-from .charnybot import CharnyBot
