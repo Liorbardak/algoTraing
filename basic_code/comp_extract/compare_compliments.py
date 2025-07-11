b321f6b2f25dc4cf8264c601d95f457a311be8d2
dadab <dadabardakcomeback@gmail.com>
2025-07-11 16:10:02 +0300

    compare GT

--- basic_code/comp_extract/compare_compliments.py
+++ basic_code/comp_extract/compare_compliments.py
@@ -205,6 +205,7 @@ def main():
     # Set paths
     basepath = '../../../data/'
     resdir = os.path.join(basepath, "results/results_20250709_220023")
+    gtdir = os.path.join(basepath, "GT")
     outputdir = os.path.join(resdir,'compareToGT')
     os.makedirs(outputdir, exist_ok=True)
 
@@ -214,7 +215,9 @@ def main():
         # Set paths
         compliments_file = os.path.join(resdir,f"{ticker}_all_validated_compliments.json")
         before_validation_file = os.path.join(resdir,f"{ticker}_detected_compliments_before_validation.json")
-        gt_file = os.path.join(basepath,f"GT/{ticker}_all_validated_compliments_4.7_GT.json")
+        #gt_file = os.path.join(basepath,f"GT/{ticker}_all_validated_compliments_4.7_GT.json")
+        gt_file = os.path.join(gtdir, f"{ticker}_all_validated_compliments_4.7_GT.json")
+
         gt_level_errors_output_file = os.path.join(outputdir,f"{ticker}_compareToGT_level_errors.json")
         name_errors_output_file = os.path.join(outputdir, f"{ticker}_compareToGT_name_errors.json")
         statistic_filename = os.path.join(outputdir, f"{ticker}_compareToGT.csv")
